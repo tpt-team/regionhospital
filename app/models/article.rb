fb643f9dab2bdd9b11ce1814612faecc047f09c2
TupickiyV <vladislav.tupickiy@gmail.com>
2017-05-20 16:37:53 +0300

    Add blog

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+end
