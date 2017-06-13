a3e18555a428aa52a8a7ee6403613f8c57bf9ac2
TupickiyV <vladislav.tupickiy@gmail.com>
2017-06-13 13:42:58 +0300

    translate allergy department page

--- app/controllers/allergy_department_controller.rb
+++ app/controllers/allergy_department_controller.rb
@@ -1,7 +1,14 @@
 class AllergyDepartmentController < ApplicationController
+  before_action :set_locale
   layout 'allergy_layout'
 
   def index; end
 
   def advice; end
+
+  private
+
+  def set_locale
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 end
