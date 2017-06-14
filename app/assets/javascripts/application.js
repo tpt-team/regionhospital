789a250e0c7fcd1d9568040935b325136ef7cf75
TupickiyV <vladislav.tupickiy@gmail.com>
2017-06-14 09:23:22 +0300

    1. Create doctor page 2. Add preview avatar for user 3. Small fix style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,16 @@
 //= require cable
 //= require cocoon
 //= require_tree .
+
+$(document).on('change', '#pictureInput', function(event) {
+  var files = event.target.files;
+  var image = files[0]
+  var reader = new FileReader();
+  reader.onload = function(file) {
+    var img = new Image();
+    console.log(file);
+    img.src = file.target.result;
+    $('#target').html(img);
+  }
+  reader.readAsDataURL(image);
+});
